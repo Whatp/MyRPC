7bf76372a67618193821506e4d133dbf3f482407
Whatp <2097266072@qq.com>
2024-12-17 10:55:02 +0800

    更新了版本1

---
+++ src/main/java/com/li/myRPCVersion1/server/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.li.myRPCVersion1.server;
+
+
+
+import com.li.myRPCVersion0.common.User;
+import com.li.myRPCVersion0.service.UserService;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserByUserId(Integer id) {
+        System.out.println("客户端查询了"+id+"的用户");
+        // 模拟从数据库中取用户的行为
+        Random random = new Random();
+        User user = User.builder().userName(UUID.randomUUID().toString())
+                .id(id)
+                .sex(random.nextBoolean()).build();
+        return user;
+    }
+    @Override
+    public Integer insertUserId(User user) {
+        System.out.println("插入数据成功："+user);
+        return 1;
+    }
+}
--- src/main/java/com/li/myRPCVersion1/service/UserService.java
+++ src/main/java/com/li/myRPCVersion1/service/UserService.java
@@ -1,9 +1,11 @@
-package com.li.myRPCVersion0.service;
+package com.li.myRPCVersion1.service;
+
 
 import com.li.myRPCVersion0.common.User;
 
 public interface UserService {
+    // 客户端通过这个接口调用服务端的实现类
     User getUserByUserId(Integer id);
-
+    // 给这个服务增加一个功能
     Integer insertUserId(User user);
 }
